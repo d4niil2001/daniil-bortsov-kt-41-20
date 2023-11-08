bf4ab6ffd8f6f20434ecb2128f42bb72dab19c22
daniil-bortsov-kt-41-20 <daniil.bortsov.01@mail.ru>
2023-11-08 21:09:36 +0300

    add logging and WeatherForecast test request

--- daniil-bortsov-kt-41-20/WeatherForecast.cs
+++ daniil-bortsov-kt-41-20/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace daniil_bortsov_kt_41_20
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
